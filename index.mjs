c296f59ef52fc6929bd5f1ee3a2e051a08d29eeb
Salil <jayinc@users.noreply.github.com>
2022-01-29 11:57:22 -0800

    stage 1

---
+++ index.mjs
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+let port = process.env.PORT || 3000;
+let URI = `mongodb+srv://atlas-monster:greek-Myth@cluster0.owvlg.mongodb.net/postDB?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("You're DB is conected");
+  })
+  .catch((e) => {
+    console.log("Error connecting to DB" + e);
+  });
+
+let userSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+let User = new mongoose.model('User', userSchema)
+
+app.get("/", (req, res) => {
+  res.send("I am your Server");
+});
+
+app.post("/post" (req, res) => {
+  res.send("You posted auccessfully!!")
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
